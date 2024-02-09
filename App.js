eed4439a4dd8857a5a2305ae0ff012646c4c4930
Shuvajit Maitra <shuvajitmaitra@gmail.com>
2024-02-10 01:10:45 +0600

    Initial project setup

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-export default function App() {
+export default function App({navigation}) {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <TouchableOpacity style={{width: 200, height: 50, backgroundColor: "#27ac1f", borderRadius: 10, textAlign: "center", alignItems: 'center', justifyContent: "center"}}>
+              <Text style={{textAlign: "center"}} onPress={() =>
+        navigation.navigate('AllNotification')
+      } >Go to All notifications</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
---
+++ app/Components/AllNotificatons.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const AllNotificatons = () => {
+  return (
+    <View>
+      <Text>AllNotificatons</Text>
+    </View>
+  )
+}
+
+export default AllNotificatons
